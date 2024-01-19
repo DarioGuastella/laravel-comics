7d9a09ec701dcb12431cd490b77f6416e8c383de
DarioGuastella <jimmyguastella@gmail.com>
2024-01-19 17:15:12 +0100

    aggiunge header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,13 @@
     <title>@yield("title", "Hello Laravel World")</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.rtl.min.css" integrity="sha512-rXhL8a+X3wTosWn2Zgd/5L8rfWrpff7qdOnl7Wg1n2zHk8lHGhiSujoyxoKw1nf43kZAzJoe0Z0ymr2Kkku7lQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/header-style.css">
 </head>
 
 <body>
 
     @include('partials.header')
+
 
     @yield("content")
 
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,36 @@
-<header>
-    <a href="{{ route('home') }}">Home</a>
+<style>
+    nav a {
+        text-decoration: none;
+        color: #555;
+        font-weight: 600;
+    }
+
+    img {
+        width: 165px;
+        height: 165px;
+    }
+
+    .active {
+        color: #0282f9;
+        border-bottom: 5px solid #0282f9;
+        line-height: 10;
+    }
+</style>
+
+<header class="d-flex container justify-content-between">
+    <!-- Logo -->
+    <img class="p-4" src="../assets/img/dc-logo.png" alt="">
+    <!-- navigatore -->
+    <nav class="d-flex align-items-center">
+        <a class="mx-3" href="#">CHARACTERS</a>
+        <a class="active mx-3" href="#">COMICS</a>
+        <a class="mx-3" href="#">MOVIES</a>
+        <a class="mx-3" href="#">TV</a>
+        <a class="mx-3" href="#">GAMES</a>
+        <a class="mx-3" href="#">COLLECTIBLES</a>
+        <a class="mx-3" href="#">VIDEOS</a>
+        <a class="mx-3" href="#">FANS</a>
+        <a class="mx-3" href="#">NEWS</a>
+        <a class="mx-3" href="#">SHOP</a>
+    </nav>
 </header>
