d3b3981776d6b41f0467135cb89b6fc6237058d8
DarioGuastella <jimmyguastella@gmail.com>
2024-01-22 10:50:13 +0100

    gestisce classi con operatore ternario

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,9 +4,10 @@
     <img class="p-4 header-logo" src="../assets/img/dc-logo.png" alt="">
     <!-- navigatore -->
     <nav class="d-flex align-items-center">
-        {{-- ToDo: ternario per classe "active" --}}
+
         @foreach ($navPages as $navPage)
-        <a class="mx-3" href="#">{{ $navPage["page"] }}</a>
+        <a class="{{($navPage["page"] == "COMICS" ? 'mx-3 active' : 'mx-3')}}"  href="#">{{ $navPage["page"] }}</a>
         @endforeach
+        
     </nav>
 </header>
